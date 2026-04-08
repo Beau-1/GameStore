f66acee8d3acc5e4effc68045a287801ae7808c9
Beau-1 <jamesbuchanan0528@gmail.com>
2026-04-08 11:30:53 -0500

    Add GameDto model and implement /games endpoint with sample data

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,41 @@
+using GameStore.Api;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+  new(
+    1,
+    "Street Fighter II",
+    "Fighting",
+    19.99m,
+    new DateOnly(1992, 7, 15)
+  ),
+  new(
+    2,
+    "The Legend of Zelda: Ocarina of Time",
+    "Action-Adventure",
+    59.99m,
+    new DateOnly(1998, 11, 21)
+  ),
+  new(
+    3,
+    "Minecraft",
+    "Sandbox",
+    26.95m,
+    new DateOnly(2011, 11, 18)
+  ),
+  new(
+    4,
+    "The Witcher 3: Wild Hunt",
+    "Action RPG",
+    39.99m,
+    new DateOnly(2015, 5, 19)
+  )
+];
+
+//GET /games
+app.MapGet("/games", () => games);
 
 app.Run();
