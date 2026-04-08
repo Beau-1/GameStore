ca712d1977e405cfae611ad2cfa1d456ea49b869
Beau-1 <jamesbuchanan0528@gmail.com>
2026-04-08 12:08:38 -0500

    Add CreateGameDto and implement /games POST endpoint for game creation

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,7 +1,8 @@
 using GameStore.Api;
+using GameStore.Api.Dtos;
 
+const string GetGameEndpointName = "GetGame";
 var builder = WebApplication.CreateBuilder(args);
-
 var app = builder.Build();
 
 List<GameDto> games = [
@@ -37,5 +38,26 @@ List<GameDto> games = [
 
 //GET /games
 app.MapGet("/games", () => games);
+
+
+//GET /games/1
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id))
+  .WithName(GetGameEndpointName);
+
+//POST /games
+app.MapPost("/games", (CreateGameDto newGame) =>
+{
+  GameDto game = new(
+  games.Count + 1,
+  newGame.Name,
+  newGame.Genre,
+  newGame.Price,
+  newGame.ReleaseDate
+  );
+  games.Add(game);
+
+  return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+
+});
 
 app.Run();
