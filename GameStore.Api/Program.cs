1d8211918cdcb869950f57dd1d47238bd17db85a
Beau-1 <jamesbuchanan0528@gmail.com>
2026-04-08 13:28:34 -0500

    Refactor GET and PUT endpoints for games; improve error handling and update HTTP request examples

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -41,24 +41,14 @@ app.MapGet("/games", () => games);
 
 
 //GET /games/1
-app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id))
+app.MapGet("/games/{id}", (int id) =>
+{
+  var game = games.Find(game => game.Id == id);
+
+  return game is null ? Results.NotFound() : Results.Ok(game);
+})
   .WithName(GetGameEndpointName);
 
-//PUT /games/1
-app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
-{
-  var index = games.FindIndex(game => game.Id == id);
-
-  games[index] = new GameDto(
-    id,
-    updatedGame.Name,
-    updatedGame.Genre,
-    updatedGame.Price,
-    updatedGame.ReleaseDate
-  );
-
-  return Results.NoContent();
-});
 
 
 
@@ -81,6 +71,31 @@ app.MapPost("/games", (CreateGameDto newGame) =>
   return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
 
 });
+
+
+//PUT /games/1
+app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
+{
+  var index = games.FindIndex(game => game.Id == id);
+
+  if (index == -1)
+  {
+    return Results.NotFound();
+  }
+
+  games[index] = new GameDto(
+    id,
+    updatedGame.Name,
+    updatedGame.Genre,
+    updatedGame.Price,
+    updatedGame.ReleaseDate
+  );
+
+  return Results.NoContent();
+});
+
+
+
 
 
 //DELETE /games/1
